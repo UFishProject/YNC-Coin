207a41aac2ab592a69ddecc81446d17ba30304bf
DraginFire <longbaby1101@qq.com>
2021-04-14 12:12:50 +0800

    AddMiningPool

--- contracts/YncCoin.sol
+++ contracts/YncCoin.sol
@@ -10,18 +10,6 @@ contract YNCCoin is ERC20Burnable, AdminRole {
         _mint(msg.sender, 1000 * 1000 * 1000 * 10**18);
     }
 
-    function mint(address recipient_, uint256 amount_)
-        public
-        onlyAdmin
-        returns (bool)
-    {
-        uint256 balanceBefore = balanceOf(recipient_);
-        _mint(recipient_, amount_);
-        uint256 balanceAfter = balanceOf(recipient_);
-
-        return balanceAfter > balanceBefore;
-    }
-
     function burn(uint256 amount) public override onlyAdmin {
         super.burn(amount);
     }
